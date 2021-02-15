817d664a0e17075051ade9d44c9022e5d26366f9
kanghongen <geneprophet>
2021-02-15 21:48:24 +0800

    2-15 21:58

--- R/infer_profiles.R
+++ R/infer_profiles.R
@@ -44,8 +44,6 @@
 #'
 #' @section Details: The modelling and mathematical details for inferring
 #'   profiles using mean-field variational inference are explained here:
-#'   \url{http://rpubs.com/cakapourani} . More specifically: \itemize{\item{For
-#'   Binomial/Bernoulli observation model check:
 #'   \url{http://rpubs.com/cakapourani/variational-bayes-bpr}
 #'
 #' @author Hongen Kang  \email{geneprophet@163.com}
